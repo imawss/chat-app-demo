7e83336f56bc042df575e0bb042884ea72679fc2
✦aras <arasarkan9@gmail.com>
2023-01-07 18:32:20 +0300

    cors access changed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userManager from "./routes/userManager.js";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
+
+app.use(cors);
 app.use(bodyParser.json());
 
 app.use('/apiv1', userManager);
