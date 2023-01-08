f80f8736c845981a4833ddf544cc8b77298c1157
✦aras <arasarkan9@gmail.com>
2023-01-08 15:05:44 +0300

    cors added

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ import cors from "cors";
 const app = express();
 const port = 8000;
 
-//app.use(cors);
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials:true, 
+    optionSuccessStatus:200
+}));
 app.use(bodyParser.json());
-
 app.use('/apiv1', userManager);
 
 app.listen(port, function() {
