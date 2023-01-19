1eb76a48249f390e71f1cd238ef3427eaa09d2ff
✦aras <arasarkan9@gmail.com>
2023-01-19 23:27:36 +0300

    error handler for message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,34 +3,41 @@ import { ref, set, push, child, remove, get, onChildAdded, onChildChanged, onChi
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { uuidv4 } from '@firebase/util';
 
-export const sendMessage = async function(req,res) {
-    const user = getAuth();
-    const messageId = uuidv4(); 
-    const senderMail = user.currentUser.email;
-    const message  = req.body;
-    const messageCreationTime = Date.now();
-    const data = {
-        messageId : messageId,
-        senderMail: senderMail,
-        message: message,
-        messageCreationTime: messageCreationTime
-    };
-    const messageDbPath = ref(database, 'messages/'+ messageId); 
-    set(messageDbPath, data);
-    res.send("Message succesfuly sent");
+export const sendMessage = async function (req, res) {
     
+    try {
+        const user = getAuth();
+        const messageId = uuidv4();
+        const senderMail = user.currentUser.email;
+        const message = req.body;
+        const messageCreationTime = Date.now();
+        const data = {
+            messageId: messageId,
+            senderMail: senderMail,
+            message: message,
+            messageCreationTime: messageCreationTime
+        };
+
+        const messageDbPath = ref(database, 'messages/' + messageId);
+        set(messageDbPath, data);
+        res.send("Message succesfuly sent");
+    } catch (error) {
+        res.send(400, { message: "User is invalid!" });
+    }
+
     //message id - sender id - message - message creation time 
 }
 
-export const getMessages = async function(req,res) {
+export const getMessages = async function (req, res) {
     get(child(dbRef, "messages/")).then(snapshot => {
         res.send(snapshot.val());
-      });
+    });
 }
 
-export const getMessageById = function(req,res){
+export const getMessageById = function (req, res) {
     get(child(dbRef, `messages/${req.params.id}`)).then(snapshot => {
-       res.json(snapshot.val());
-})}
+        res.json(snapshot.val());
+    })
+}
 
 //ATTENTION! *WIP*
